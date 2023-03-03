ea114cab96e0fedf301843aba1f09b75f3887ac6
hp <engrziaurrehman.kicsit@gmail.com>
2023-03-04 00:06:06 +0500

    upd

--- 03.ComparisonP1.py
+++ 03.ComparisonP1.py
@@ -13,8 +13,14 @@ for j in range(1000000):
 
 endTime = os.times()
 
+print(f"User CPU Start Time: {startTime.user}")
+# print(f"System CPU Start Time: {startTime.system}")
+
+print(f"User CPU End Time: {endTime.user}")
+# print(f"System CPU End Time: {endTime.system}")
+
 totalUserTime = endTime.user - startTime.user
 totalSystemTime = endTime.system - startTime.system
 
-print(f"User Time: {totalUserTime}")
-print(f"System Time: {totalSystemTime}")
+print(f"User CPU Time: {totalUserTime}")
+print(f"System CPU Time: {totalSystemTime}")
--- 04.ComparisonP2.py
+++ 04.ComparisonP2.py
@@ -8,22 +8,41 @@ size = MPI.COMM_WORLD.size
 # Print the rank and size of the communicator
 print(f"I am process {rank} of {size}")
 
-startTime = os.times()
-
 if rank == 0:
     result = 0
+    startTime = os.times()
     for i in range(1000000):
         result = i*i
+    endTime = os.times()
+    print(f"User CPU Start Time: {startTime.user}")
+    # print(f"System CPU Start Time: {startTime.system}")
+
+    print(f"User CPU End Time: {endTime.user}")
+    # print(f"System CPU End Time: {endTime.system}")
+
+    totalUserTime = endTime.user - startTime.user
+    totalSystemTime = endTime.system - startTime.system
+
+    print(f"User CPU Time: {totalUserTime}")
+    print(f"System CPU Time: {totalSystemTime}")
+
 
 if rank == 1:
+    
     result1 = 0
+    startTime = os.times()
     for j in range(1000000):
         result1 = j*j
 
-endTime = os.times()
-       
-totalUserTime = endTime.user - startTime.user
-totalSystemTime = endTime.system - startTime.system
+    endTime = os.times()
+    print(f"User CPU Start Time: {startTime.user}")
+    # print(f"System CPU Start Time: {startTime.system}")
 
-print(f"User Time: {totalUserTime}")
-print(f"System Time: {totalSystemTime}")
+    print(f"User CPU End Time: {endTime.user}")
+    # print(f"System CPU End Time: {endTime.system}")
+
+    totalUserTime = endTime.user - startTime.user
+    totalSystemTime = endTime.system - startTime.system
+
+    print(f"User Time: {totalUserTime}")
+    print(f"System Time: {totalSystemTime}")
