43c481af482141b2a9cb84d3df664e46164f5cb8
hp <engrziaurrehman.kicsit@gmail.com>
2023-03-06 23:16:41 +0500

    update 09

--- 09.DataScatterAndGather.py
+++ 09.DataScatterAndGather.py
@@ -8,19 +8,39 @@ size = MPI.COMM_WORLD.size
 data = []
 for i in range(size):
     data.append(i*2)
+
 # Scatter the data to all the processes
 local_data = MPI.COMM_WORLD.scatter(data, root=0)
 
-if rank == 1:
-    print(local_data)
-    local_data += 3
 # Perform some computation on the local data
-result =sum([local_data])
-print(result)
+if rank == 0:
+    print(f"Local Data {local_data} given to Process {rank}")
+    local_data += 8
+    print(f"Local Data updated to {local_data} of Process {rank}")
+
+if rank == 1:
+    print(f"Local Data {local_data} given to Process {rank}")
+    local_data += 3
+    print(f"Local Data updated to {local_data} of Process {rank}")
+
+if rank == 2:
+    print(f"Local Data {local_data} given to Process {rank}")
+    local_data += 1
+    print(f"Local Data updated to {local_data} of Process {rank}")
+
+if rank == 3:
+    print(f"Local Data {local_data} given to Process {rank}")
+    local_data += 5
+    print(f"Local Data updated to {local_data} of Process {rank}")
+
+
+# result =local_data
+# print(result)
+
 # Gather the results from all the processes
-results = MPI.COMM_WORLD.gather(result, root=0)
+results = MPI.COMM_WORLD.gather(local_data, root=0)
 
 # Print the results on the root process
 if rank == 0:
-    print(data)
-    print(f"Results: {results}")
+    print(f"Local Data before Scattered {data}")
+    print(f"Results: after gathering {results}")
