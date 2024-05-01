01bc206a0aa7b03bc2690e776c57f7858f18ec20
Zia Ur Rehman <77435711+ZiaUrRehman-bit@users.noreply.github.com>
2024-05-01 15:17:42 +0500

    updated

--- 19.WebcameExample.py
+++ 19.WebcameExample.py
@@ -6,8 +6,8 @@ from ultralytics.utils.plotting import colors, Annotator
 model = YOLO("yolov8s.pt")
 names = model.model.names
 
-cam1 = cv2.VideoCapture(0)
-cam2 = cv2.VideoCapture(1)
+cam1 = cv2.VideoCapture(2)
+cam2 = cv2.VideoCapture(0)
 
 cTime = 0
 pTime = 0
